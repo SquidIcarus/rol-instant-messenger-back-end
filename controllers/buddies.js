b1310b128aca82c2d3c813cdd02cfcbd8286e5ae
User Name <landonisaiah@gmail.com>
2025-09-07 20:03:30 -0700

    add buddy route created, tested with postman

--- controllers/buddies.js
+++ controllers/buddies.js
@@ -6,6 +6,8 @@ const User = require('../models/User');
 
 module.exports = router;
 
+// buddy list route 
+
 router.get('/', verifyToken, async (req, res) => {
     try {
         const buddies = await Buddy.find({
@@ -14,6 +16,37 @@ router.get('/', verifyToken, async (req, res) => {
         });
 
         res.json(buddies);
+    } catch (err) {
+        res.status(500).json({ err: err.message });
+    }
+});
+
+// Add buddy route
+
+router.post('/', verifyToken, async (req, res) => {
+    try {
+        const { friend_screen_name } = req.body;
+
+        const friendUser = await User.findOne({ screen_name: friend_screen_name });
+        if (!friendUser) {
+            return res.status(404).json({ err: 'User not found.' });
+        }
+
+        if (friendUser._id.toString() === req.user._id) {
+            return res.status(400).json({ err: 'Cannot add yourself as a buddy.' });
+        }
+
+        const newBuddy = new Buddy({
+            user_id: req.user._id,
+            friend_user_id: friendUser._id
+        });
+
+        await newBuddy.save();
+
+        res.status(201).json({
+            message: 'Buddy added succesfully.',
+            buddy: newBuddy
+        });
     } catch (err) {
         res.status(500).json({ err: err.message });
     }
